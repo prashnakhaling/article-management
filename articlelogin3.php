31047eeb2702c847a557d121ad2f74c7ab46660c
prashnakhaling <prashna.krai@gmail.com>
2024-04-17 22:31:25 +0545

    image upload

--- articlelogin3.php
+++ articlelogin3.php
@@ -5,28 +5,34 @@ if (isset($_POST['add'])) {
     $content = $_POST['content'];
     $image_file = $_FILES['image']['name'];
     $tempname = $_FILES['image']['tmp_name'];
-    $folder = 'Images/';
+    $folder = 'Images/'.$image_file;
     $path = $folder.$image_file;
 
-    if (empty($title) || empty($content) || empty($image)) {
+    if (empty($title) || empty($content) || empty($image_file)) {
         echo "please fill the details to add your articles.";
     } else if (strlen($title) < 5) {
         echo "title should be 5 characters long.";
     } else
 
-        $sql = "INSERT INTO articlesdetails (title, content, image) values ('$title', '$content' , '$image')";
-    $data = mysqli_query($connectin, $sql);
-
-    // mysqli_query($connectin, $sql);
-    if (mysqli_connect_errno()) {
-        die("Failed to connect:") . mysqli_connect_error();
-        exit();
-
-    } else {
+        $sql = "INSERT INTO articlesdetails (title, content, image) values ('$title', '$content' , '$image_file')";
+       $data = mysqli_query($connectin, $sql);
+        if (move_uploaded_file($tempname, $path)) {
         header("Location:addarticle4.php");
-        exit();
+            
+        }else{
+            echo "file not uploaded";
+        }
 
-    }
+
+    // if (mysqli_connect_errno()) {
+    //     die("Failed to connect:") . mysqli_connect_error();
+    //     exit();
+
+    // } else {
+    //     header("Location:addarticle4.php");
+    //     exit();
+
+    // }
 
 }
 
